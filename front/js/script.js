c36d1dd19d43cbfafc86aeb96b3e4c63644f96f6
Lucas Balland <lucasballand101@gmail.com>
2022-10-04 08:50:21 +0200

    Page principal utilisation de foreach pour la dupli
    Page principal utilisation de foreach pour la dupli

--- front/js/script.js
+++ front/js/script.js
@@ -1,16 +1,65 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        const imageUrl = data[0].imageUrl
-        console.log("url de l'image", imageUrl)
+    .then((data) => addProducts(data));
 
-        const anchor = document.createElement('a')
-        anchor.href = imageUrl
-        anchor.text = "Un super KANAP"
-        const items = document.querySelector("#items")
-        if (items != null) {
-            items.appendChild(anchor)
-            console.log("Les liens on été chargés")
-        }
-    })
+function addProducts(data) {
+    // const _id = data[0]._id
+    // const imageUrl = data[0].imageUrl
+    // const altTxt = data[0].altTxt
+    // const name  = data[0].name
+    // const description = data[0].description
+
+    data.forEach((KANAP) => {
+
+        const { _id, imageUrl, altTxt, name, description } = KANAP
+
+        const anchor = makeAnchor(_id);
+
+        const image = MakeImage(imageUrl, altTxt)
+        const article = document.createElement("article")
+        const h3 = MakeH3(name);
+        const p = MakePragraph(description);
+
+        article.appendChild(image)
+        article.appendChild(h3)
+        article.appendChild(p)
+        AppendArticleToAnchor(anchor, article);
+    });
+}
+
+function makeAnchor(id) {
+    const anchor = document.createElement('a')
+    anchor.href = "./product.html?id=42" + id
+    return anchor
+}
+
+function AppendArticleToAnchor(anchor, article) {
+    const items = document.querySelector("#items")
+    if (items != null) {
+        items.appendChild(anchor)
+        anchor.appendChild(article)
+    }
+}
+
+function MakeImage(imageUrl, altTxt) {
+    const image = document.createElement("img")
+    image.src = imageUrl
+    image.alt = altTxt
+    image.removeAttribute("style")
+    image.removeAttribute("title")
+    return image
+}
+
+function MakeH3(name) {
+    const h3 = document.createElement("h3")
+    h3.textContent = name
+    h3.classList.add("productName")
+    return h3
+}
+
+function MakePragraph(description) {
+    const p = document.createElement("p")
+    p.textContent = description
+    p.classList.add("productDescription")
+    return p
+}
