a2da0c6638c8e92706d0b6c9e85ac7393e4094b5
Lucas Balland <lucasballand101@gmail.com>
2022-10-04 10:29:49 +0200

    Local host bug
    Local host bug

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,8 @@
-const querryString = window.location.search
-const urlParams = new URLSearchParams(querryString)
-const id = urlParams.get("id")
-console.log({id}) 
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+const id = urlParams.get('id')
+console.log({id})
 
-fetch("http://localhost:3000/api/products/${id}")
-.then((res) => res.json())
-.then((res) => console.log(res))
+fetch ('http://localhost:3000/api/products/${id}')
+.then ((response) => response.json())
+.then ((res) => console.log(res))
