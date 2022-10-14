a1c1bbf1c1b64d70bc52e25501dcf14ae78580f6
Lucas Balland <lucasballand101@gmail.com>
2022-10-14 10:39:59 +0200

    Ajouts commentaire
    Ajouts commentaires dans le fichier confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,3 +1,5 @@
+// génération numéru de commande pour le client a la fin de la commande
+
 function main(){
     const idNode = document.getElementById("orderId");
     idNode.innerText = localStorage.getItem("orderId");
